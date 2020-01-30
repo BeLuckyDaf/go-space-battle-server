c79293c421f3b3c1c76b8b32e37338b6fbaf41d5
BeLuckyDaf <beluckydaf@gmail.com>
2020-01-30 15:16:50 +0300

    Add TODOs and update test.

--- player.go
+++ player.go
@@ -12,7 +12,7 @@ const (
 // Player is used as a general representation of a player
 type Player struct {
 	Username string `json:"username"`
-	Token    string `json:"-"`
+	Token    string `json:"-"` // token is not sent on /players
 	Power    int    `json:"power"`
 	Hp       int    `json:"hp"`
 	Location int    `json:"location"`
